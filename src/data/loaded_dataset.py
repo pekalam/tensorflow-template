4bf36742aa7c33b8144d1c9d9909583c15abcc3d
Marek <marekbf3@gmail.com>
2021-05-28 14:26:17 +0200

    added prev ds

--- src/data/loaded_dataset.py
+++ src/data/loaded_dataset.py
@@ -3,13 +3,15 @@ import tensorflow as tf
 class LoadedDataset():
     """Contains dataset loaded by calling DatasetLoader object __call__ method
     """
-    def __init__(self, x, y, x_val = None, y_val = None, x_test = None, y_test = None):
+    def __init__(self, x, y, x_val = None, y_val = None, x_test = None, y_test = None, x_prev = None, y_prev = None):
         self._x = x
         self._y = y
         self._x_val = x_val
         self._y_val = y_val
         self._x_test = x_test
         self._y_test = y_test
+        self._x_prev = x_prev
+        self._y_prev = y_prev
 
     @property
     def x(self) -> tf.data.Dataset: return self._x
@@ -28,3 +30,9 @@ class LoadedDataset():
     
     @property
     def y_test(self) -> tf.data.Dataset: return self._y_test
+
+    @property
+    def x_prev(self): return self._x_prev
+    
+    @property
+    def y_prev(self): return self._y_prev
