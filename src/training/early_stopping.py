c945b00366d80edfa8f79b9b5fdd97b23b6c65bc
Marek <marekbf3@gmail.com>
2021-06-01 20:49:19 +0200

    new early stop impl

--- src/training/early_stopping.py
+++ src/training/early_stopping.py
@@ -2,11 +2,16 @@ from typing import Tuple
 
 
 class EarlyStopping:
-    def __init__(self, early_stop_patience):
+    def __init__(self, early_stop_patience, min_delta = None, early_stop_min_delta_patience = None):
         self.inc_start_loss = 0.
         self.prev_metric_val = float("inf")
         self.inc_iter = 0
+        self.min_delta_iter = 0
         self.early_stop_patience = early_stop_patience
+        self.early_stop_min_delta_patience = early_stop_min_delta_patience if early_stop_min_delta_patience is not None else float("inf")
+        self.min_delta = min_delta if min_delta is not None else float("-inf")
+        assert early_stop_patience >= 0
+        assert (early_stop_min_delta_patience is None and min_delta is None) or (early_stop_min_delta_patience >= 0 and min_delta > 0)
     
     def add_metric(self, metric_val: float) -> Tuple[bool, bool]:
         """[summary]
@@ -18,20 +23,38 @@ class EarlyStopping:
             Tuple[bool, bool]: (should_checkpoint, should_stop)
         """
         should_checkpoint = should_stop = False
-        if metric_val >= self.prev_metric_val:
+
+        if self.inc_iter > self.early_stop_patience or self.min_delta_iter > self.early_stop_min_delta_patience:
+            raise ValueError("Training not stopped")
+
+        if metric_val > self.prev_metric_val:
+            self.min_delta_iter = 0
             self.inc_iter += 1
+            print('Early stop loss increased')
             if self.inc_iter == 1:
-                print('Saving checkpoint due to increase of val loss')
+                print('Saving checkpoint due to increase of early stop loss')
                 should_checkpoint = True
-                self.inc_start_loss = metric_val
+                self.inc_start_loss = self.prev_metric_val
             if self.inc_iter > self.early_stop_patience:
                 should_stop = True
-                print('Stopped due to increase of val loss')
+                print('Stopped due to increase of early stop loss')
 
-        if self.inc_iter > 1 and metric_val < self.inc_start_loss:
+        if self.inc_iter > 1 and metric_val <= self.inc_start_loss:
             self.inc_iter = 0
-            print('saving checkpoint due to decrease of val loss')
+            self.inc_start_loss = 0
+            print('saving checkpoint due to decrease of early stop loss')
             should_checkpoint = True
+        
+
+        if metric_val <= self.prev_metric_val and self.prev_metric_val - metric_val < self.min_delta:
+            self.min_delta_iter += 1
+            print('Early stop loss delta < ', self.min_delta)
+            if self.min_delta_iter > self.early_stop_min_delta_patience:
+                should_stop = True
+                should_checkpoint = True
+        else:
+            self.min_delta_iter = 0
+
 
         self.prev_metric_val = metric_val
         return (should_checkpoint, should_stop)
--- tests/test_early_stopping.py
+++ tests/test_early_stopping.py
@@ -1,21 +1,85 @@
 from training.early_stopping import EarlyStopping
+import pytest
 
 def test_early_stopping_return_values():
-    early_stopping = EarlyStopping(3)
+    early_stopping = EarlyStopping(2, 0.00001, 2000)
     assert early_stopping.metric_increased == False
 
     (checkpoint, stop) = early_stopping.add_metric(0.1)
-    assert early_stopping.metric_increased == False and stop == False and checkpoint == False
+    assert early_stopping.metric_increased == False and stop == False and checkpoint == False and early_stopping.min_delta_iter == 0 and early_stopping.inc_iter == 0 and early_stopping.inc_start_loss == 0
     
+    # inc1
     (checkpoint, stop) = early_stopping.add_metric(0.2)
-    assert early_stopping.metric_increased == True and stop == False and checkpoint == True
+    assert early_stopping.metric_increased == True and stop == False and checkpoint == True and early_stopping.min_delta_iter == 0 and early_stopping.inc_iter == 1 and early_stopping.inc_start_loss == 0.1
 
+    # should not be counted
     (checkpoint, stop) = early_stopping.add_metric(0.2)
-    assert early_stopping.metric_increased == True and stop == False and checkpoint == False
+    assert early_stopping.metric_increased == True and stop == False and checkpoint == False and early_stopping.min_delta_iter == 1 and early_stopping.inc_iter == 1 and early_stopping.inc_start_loss == 0.1
+
+    # inc2 - min_delta_iter reset
+    (checkpoint, stop) = early_stopping.add_metric(0.3)
+    assert early_stopping.metric_increased == True and stop == False and checkpoint == False and early_stopping.min_delta_iter == 0 and early_stopping.inc_iter == 2 and early_stopping.inc_start_loss == 0.1
+    
+    # inc3
+    (checkpoint, stop) = early_stopping.add_metric(0.4)
+    assert early_stopping.metric_increased == True and stop == True and checkpoint == False and early_stopping.min_delta_iter == 0 and early_stopping.inc_iter == 3 and early_stopping.inc_start_loss == 0.1
+
+    #should throw
+    with pytest.raises(ValueError):
+        (checkpoint, stop) = early_stopping.add_metric(0.1)
+    assert early_stopping.metric_increased == True and early_stopping.min_delta_iter == 0 and early_stopping.inc_iter == 3 and early_stopping.inc_start_loss == 0.1
+
+
+def test_early_stopping_stops_when_loss_smaller_than_min_delta_after_min_delta_patience_epochs():
+    early_stopping = EarlyStopping(3, 0.01, 2)
+    assert early_stopping.metric_increased == False
 
     (checkpoint, stop) = early_stopping.add_metric(0.1)
-    assert early_stopping.metric_increased == False and stop == False and checkpoint == True
+    assert early_stopping.metric_increased == False and stop == False and checkpoint == False and early_stopping.min_delta_iter == 0 and early_stopping.inc_iter == 0
 
+    # delta #1
+    (checkpoint, stop) = early_stopping.add_metric(0.095)
+    assert early_stopping.metric_increased == False and stop == False and checkpoint == False and early_stopping.min_delta_iter == 1 and early_stopping.inc_iter == 0
+
+    # delta #2
+    (checkpoint, stop) = early_stopping.add_metric(0.094)
+    assert early_stopping.metric_increased == False and stop == False and checkpoint == False and early_stopping.min_delta_iter == 2 and early_stopping.inc_iter == 0   
+    
+    # delta #3 - should stop and checkpoint
+    (checkpoint, stop) = early_stopping.add_metric(0.094)
+    assert early_stopping.metric_increased == False and stop == True and checkpoint == True and early_stopping.min_delta_iter == 3 and early_stopping.inc_iter == 0
+
+    #should throw
+    with pytest.raises(ValueError):
+        (checkpoint, stop) = early_stopping.add_metric(0.094)
+
+
+def test_early_stopping_when_loss_reach_base_value_returns_checkpoint():
+    early_stopping = EarlyStopping(2, 0.00001, 2000)
+    assert early_stopping.metric_increased == False
+
+    (checkpoint, stop) = early_stopping.add_metric(0.1)
+    assert early_stopping.metric_increased == False and stop == False and checkpoint == False and early_stopping.min_delta_iter == 0 and early_stopping.inc_iter == 0 and early_stopping.inc_start_loss == 0
+    
+    # inc1
     (checkpoint, stop) = early_stopping.add_metric(0.2)
-    assert early_stopping.metric_increased == True and stop == False and checkpoint == True
+    assert early_stopping.metric_increased == True and stop == False and checkpoint == True and early_stopping.min_delta_iter == 0 and early_stopping.inc_iter == 1 and early_stopping.inc_start_loss == 0.1
 
+    # inc2
+    (checkpoint, stop) = early_stopping.add_metric(0.3)
+    assert early_stopping.metric_increased == True and stop == False and checkpoint == False and early_stopping.min_delta_iter == 0 and early_stopping.inc_iter == 2 and early_stopping.inc_start_loss == 0.1
+
+    # base val reached
+    (checkpoint, stop) = early_stopping.add_metric(0.1)
+    assert early_stopping.metric_increased == False and stop == False and checkpoint == True and early_stopping.min_delta_iter == 0 and early_stopping.inc_iter == 0 and early_stopping.inc_start_loss == 0
+
+
+
+def test_early_stopping_min_delta_default_params():
+    early_stopping = EarlyStopping(2)
+
+    (checkpoint, stop) = early_stopping.add_metric(0.1)
+    assert early_stopping.metric_increased == False and stop == False and checkpoint == False and early_stopping.min_delta_iter == 0 and early_stopping.inc_iter == 0 and early_stopping.inc_start_loss == 0
+
+    (checkpoint, stop) = early_stopping.add_metric(0.1)
+    assert early_stopping.metric_increased == False and stop == False and checkpoint == False and early_stopping.min_delta_iter == 0 and early_stopping.inc_iter == 0 and early_stopping.inc_start_loss == 0
